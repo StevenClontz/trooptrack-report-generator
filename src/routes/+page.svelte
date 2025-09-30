e1f80544d2868ea1a5609d6cd6f77e5bfe9c0b45
Steven Clontz <steven.clontz@gmail.com>
2025-09-30 14:25:06 +0000

    use papaparse to parse CSV

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,60 @@
 <script lang="ts">
+    import Papa from 'papaparse';
     let csvFile: File | null = null;
+    let csvData: Record<string, string>[] | null = null;
+    const KEYS = [
+        "Account name",
+        "Activity on",
+        "Value",
+        "Description",
+        "Details",
+        "Balance",
+    ]
 
     function handleFileUpload(event: Event) {
         const input = event.target as HTMLInputElement;
         if (input.files && input.files.length > 0) {
             csvFile = input.files[0];
+            Papa.parse(csvFile, {
+                header: true,
+                complete: function(results) {
+                    csvData = results.data as Record<string, string>[];
+                }
+            });
         }
     }
 </script>
 
 <h1 class="text-3xl font-bold text-center mt-8 mb-4 text-blue-700">TroopTrack Money Account Report Generator</h1>
 
-{#if csvFile}
-    <div class="max-w-2xl mx-auto my-6 p-4 bg-gray-100 rounded shadow">
+{#if csvData}
+    <div class="mx-auto my-6 p-4 bg-gray-100 rounded shadow">
         <h2 class="text-xl font-semibold mb-2">CSV File Contents</h2>
-        <pre class="overflow-x-auto whitespace-pre-wrap">
-            {#await csvFile.text() then text}
-                {text}
-            {/await}
-        </pre>
+        <table>
+            <thead>
+                <tr>
+                    {#if csvData.length > 0}
+                        {#each KEYS as header}
+                            <th class="border px-2 py-1 bg-gray-200">{header}</th>
+                        {/each}
+                    {/if}
+                </tr>
+            </thead>
+            <tbody>
+                {#each csvData.splice(0, 10) as row}
+                    <tr>
+                        {#each KEYS as key}
+                            <td class="border px-2 py-1">{row[key]}</td>
+                        {/each}
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
     </div>
 {:else}
+    <p>
+        Visit your TroopTrack Money Book page and export the report as a CSV file to upload here.
+    </p>
     <div class="flex justify-center my-6">
         <label class="cursor-pointer bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
             Upload CSV
