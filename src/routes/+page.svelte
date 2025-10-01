72837d9d3b597d1a079f89b9a88484bdb246a6a4
Steven Clontz <steven.clontz@gmail.com>
2025-10-01 17:47:08 -0500

    add link to GitHub

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -50,5 +50,8 @@
         (All data is processed locally in your browser and not uploaded anywhere.)
     </p>
     <InputFile bind:csvData />
+    <p>
+        Source: <a class="underline text-blue-800" href="https://github.com/StevenClontz/trooptrack-report-generator">GitHub</a>
+    </p>
 {/if}
 
