a9f6146f0c7e9db5a47508f7b3cd0614eecdd58a
Steven Clontz <steven.clontz@gmail.com>
2025-10-01 01:40:10 +0000

    working datepicker

--- src/lib/DatePicker.svelte
+++ src/lib/DatePicker.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   export let startDate = new Date();
   export let endDate = new Date();
-  let isOpen = false;
-
-  const onClearDates = () => {
-    startDate = new Date();
-    endDate = new Date();
-  };
-
-  const toggleDatePicker = () => (isOpen = !isOpen);
+  let startDateString = startDate.toISOString().substring(0, 10);
+  let endDateString = endDate.toISOString().substring(0, 10);
+  $: startDate = new Date(startDateString);
+  $: endDate = new Date(endDateString);
 </script>
+
+Start:
+<input type="date" bind:value={startDateString} class="border px-2 py-1 rounded mr-2"/>
+
+End:
+<input type="date" bind:value={endDateString} class="border px-2 py-1 rounded mr-2"/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,20 +5,28 @@
     import InputFile from '$lib/InputFile.svelte';
     let csvData: Record<string, string>[] | null = null;
     let chosenAccountNames: string[] = [];
-    let startDate: Date = new Date();
-    let endDate: Date = new Date();
+    let startDate: Date = new Date(new Date().getFullYear(), new Date().getMonth() - 1, 1);
+    let endDate: Date = new Date(new Date().getFullYear(), new Date().getMonth(), 0);
     let showReport = false;
     const runReport = () => {
+        if (chosenAccountNames.length === 0) {
+            alert("Please select at least one account.");
+            return;
+        }
+        if (endDate < startDate) {
+            alert("Please select an end date no earlier than the start date.");
+            return;
+        }
         showReport = true;
     }
 </script>
 
-{#if chosenAccountNames.length > 0 && showReport}
-    <button class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700" on:click={_=>showReport = false}>Reset Report</button>
+{#if showReport}
+    <button class="print:hidden bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700" on:click={_=>showReport = false}>Reset Report</button>
 {:else if csvData}
     <AccountSelect {csvData} bind:chosenAccountNames />
     <DatePicker bind:startDate bind:endDate />
-    <button class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700" on:click={runReport}>Run Report</button>
+    <button class="bg-blue-600 text-white px-4 py-2 mt-4 rounded hover:bg-blue-700" on:click={runReport}>Run Report</button>
     <div class="mx-auto my-6 p-4 bg-gray-100 rounded shadow">
         <h2 class="text-xl font-semibold mb-2">CSV File Contents (first 10 rows)</h2>
         <CsvTable {csvData} rows={10} />
