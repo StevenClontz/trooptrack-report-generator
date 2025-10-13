582de0b5a8870e36a9601254eb7d48945c32b6a1
Steven Clontz <steven.clontz@gmail.com>
2025-10-13 11:37:03 -0500

    Remove display of startDateString in DatePicker

--- src/lib/DatePicker.svelte
+++ src/lib/DatePicker.svelte
@@ -5,7 +5,6 @@
 
 Start:
 <input type="date" bind:value={startDateString} class="border px-2 py-1 rounded mr-2"/>
-{startDateString}
 
 End:
 <input type="date" bind:value={endDateString} class="border px-2 py-1 rounded mr-2"/>
